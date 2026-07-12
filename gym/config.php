32385df2fea8ddfd204b67a8cfb8d9e118b7ac66
sahil-korotania <sahilkorotania51@gmail.com>
2026-07-12 11:12:06 +0530

    h

--- gym/config.php
+++ gym/config.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect("localhost","root","","gym");
+	$con=mysqli_connect("localhost","root","sahil123","sahil");
 	if(!$con)
 	{
 		echo "<script>window.location.assign('login.php?msg=Error while establishing connection with database')</script>";
